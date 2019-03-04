fc32861e68086c2397d938a79b4c821769b93842
kitri <kitri@kitri-PC>
2019-03-04 17:55:47 +0900

    DB AWS에 연결

--- kokkok/src/com/kokkok/util/DBConnection.java
+++ kokkok/src/com/kokkok/util/DBConnection.java
@@ -24,7 +24,7 @@ public class DBConnection {
 	
 	
 	public static Connection makeConnection() throws SQLException {	
-	return DriverManager.getConnection("jdbc:oracle:thin:@192.168.18.33:1521:xe","kitri","kitri");
+	return DriverManager.getConnection("jdbc:oracle:thin:@oracle-instance1.c39btlcttohl.ap-northeast-2.rds.amazonaws.com:1521:orcl","kitri","kitri");
 	
 	}
 }
